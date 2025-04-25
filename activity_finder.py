3b2eac76e4ccfaf6817d0ce548a716cf10735dcd
Keith Fike <keith.fike@lvt.com>
2025-04-25 14:31:23 -0600

    cut locator finds gaps, gap length is variable

--- activity_finder.py
+++ activity_finder.py
@@ -6,17 +6,19 @@ from frame_collector import getFrames
 
 def findActivity(frames_dict):
     PKT_SIZE_THRESHOLD = 100
+
+    frames_to_keep_dict = {}
     
-    #append 1 to frame's data list if below packet size threshold
     for frame in frames_dict:
-        if int(frames_dict[frame][1]) < PKT_SIZE_THRESHOLD:
-            frames_dict[frame].append(1)
-        else:
-            frames_dict[frame].append(0)
+        key_num = int(frame)
+        if int(frames_dict[frame][1]) > PKT_SIZE_THRESHOLD:
+            frames_to_keep_dict.update({key_num: frames_dict[frame]})
+    
+    return frames_to_keep_dict
 
 if __name__ == "__main__":
     frames_dict = getFrames("test.mp4")
 
-    frames_dict = findActivity(frames_dict)
+    frames_to_keep_dict = findActivity(frames_dict)
 
-    print(json.dumps(frames_dict))
+    print(json.dumps(frames_to_keep_dict))
